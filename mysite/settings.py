81c204de740c916690e89eefa7f868d9d603de4a
Adam Ryan <thestormguy33@gmail.com>
2024-12-12 09:42:38 +0000

    fit sslmodeissue

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,12 +82,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',  # Default to SQLite
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
