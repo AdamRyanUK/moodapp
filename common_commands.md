0df65d57a90db586a360dc60f385d051b3db596a
Adam Ryan <thestormguy33@gmail.com>
2024-12-12 12:28:07 +0000

    added the dual environments for both local and prod

--- common_commands.md
+++ common_commands.md
@@ -7,3 +7,18 @@ git push
 dev server: http://127.0.0.1:8000
 
 api key: d6duuiqm1wlscqmf8e6a4v3y91pugctik2uw9ici
+
+Heroku app name: Clearskyapp
+login : heroku login
+
+*Commit to both heroku and github*
+
+git add .
+git commit -m "Message"
+git push origin main # github
+git push heroku main # heroku
+
+*run database migrations*
+heroku run python manage.py migrate
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,7 @@
 import os
 import django_heroku
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,9 +82,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+if os.getenv('HEROKU'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
