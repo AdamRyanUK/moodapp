544686376c04e5439f24672c3a68f146db3dade7
Adam Ryan <thestormguy33@gmail.com>
2024-12-31 22:05:59 +0000

    sorting out more mobile view

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -7,3 +7,13 @@ def mood_score_color(mood_score):
     red = 255 - (mood_score * 25)
     green = 255 - ((10 - mood_score) * 25)
     return f'rgb({red},{green},0)'
+
+@register.filter
+def duration_in_hours_minutes(value):
+    if value:
+        total_seconds = int(value.total_seconds())
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        return f"{hours}h {minutes}m"
+    return value
+
