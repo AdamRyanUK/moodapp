abf1422da92ece220b79500c6729416dbe3ea852
Adam Ryan <thestormguy33@gmail.com>
2025-03-24 12:15:57 +0000

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,6 +157,20 @@ WAGTAIL_SITE_NAME = "ClearSky"  # Nom de ton site, mets ce que tu veux
 WAGTAIL_APPEND_SLASH = False
 DEBUG = True
 
+# Configuration AWS à partir des variables d'environnement
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')  # Récupère 'clearskyappfr' depuis Heroku
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')  # Valeur par défaut si non définie
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Configuration des médias pour Wagtail/Django
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+MEDIA_ROOT = ''  # Pas besoin avec S3
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
