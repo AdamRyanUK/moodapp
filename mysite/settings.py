05588a3852aaca1e0bffee005c8d15ac4505ed41
Adam Ryan <thestormguy33@gmail.com>
2025-03-24 13:43:30 +0000

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -170,6 +170,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 MEDIA_ROOT = ''  # Pas besoin avec S3
+WAGTAILIMAGES_IMAGE_MODEL = 'wagtailimages.Image'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
