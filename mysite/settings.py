8eb96f55d56c18518beae122f123eabadbb8a961
Adam Ryan <thestormguy33@gmail.com>
2025-03-24 12:51:20 +0000

    changed a line to see if new system works

--- mysite/settings.py
+++ mysite/settings.py
@@ -165,7 +165,7 @@ AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')  # Valeur
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-#changed vars need to push again
+
 # Configuration des médias pour Wagtail/Django
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
